4afa25455996fd38ab8d2f4ecabdfd6fefae0276
Dmitrii Zinovev <zinovev_dm@mail.ru>
2026-07-31 01:09:26 +0300

    create user in main

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -8,8 +8,11 @@ import (
 	"syscall"
 
 	core_logger "github.com/zinovev-dm/golang-todoapp/internal/core/logger"
+	core_repository_postgres_pool "github.com/zinovev-dm/golang-todoapp/internal/core/repository/postgres/pool"
 	core_http_middleware "github.com/zinovev-dm/golang-todoapp/internal/core/transport/http/middleware"
 	core_http_server "github.com/zinovev-dm/golang-todoapp/internal/core/transport/http/server"
+	users_postgres_repository "github.com/zinovev-dm/golang-todoapp/internal/features/users/repository/postgres"
+	users_service "github.com/zinovev-dm/golang-todoapp/internal/features/users/service"
 	users_transport_http "github.com/zinovev-dm/golang-todoapp/internal/features/users/transport/http"
 	"go.uber.org/zap"
 )
@@ -29,14 +32,22 @@ func main() {
 	}
 	defer logger.Close()
 
-	logger.Debug("starting todo application")
+	logger.Debug("initializing Postgres connection pool")
 
-	userTransportHTTP := users_transport_http.NewUsersHTTPHandler(nil)
+	pool, err := core_repository_postgres_pool.NewConnectionPool(ctx, *core_repository_postgres_pool.NewConfigMust())
 
-	usersRouters := userTransportHTTP.Routes()
+	if err != nil {
+		logger.Fatal("failed to init Postgres connection pool: %w", zap.Error(err))
+	}
+	defer pool.Close()
 
-	apiVersionRouter := core_http_server.NewAPIVersionRouter(core_http_server.APIVersionV1)
-	apiVersionRouter.RegisterRoutes(usersRouters...)
+	logger.Debug("initializing feature", zap.String("feature", "users"))
+
+	usersRepository := users_postgres_repository.NewUsersRepository(pool)
+	usersService := users_service.NewUsersService(usersRepository)
+	usersTransportHTTP := users_transport_http.NewUsersHTTPHandler(usersService)
+
+	logger.Debug("initializing HTTP server")
 
 	httpServer := core_http_server.NewHTTPServer(
 		core_http_server.NewConfigMust(),
@@ -46,6 +57,9 @@ func main() {
 		core_http_middleware.Panic(),
 		core_http_middleware.Trace(),
 	)
+
+	apiVersionRouter := core_http_server.NewAPIVersionRouter(core_http_server.APIVersionV1)
+	apiVersionRouter.RegisterRoutes(usersTransportHTTP.Routes()...)
 
 	httpServer.RegisterAPIRoutes(apiVersionRouter)
 
